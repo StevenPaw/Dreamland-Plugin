55edb9d110042e2b2e4b35fb2b0a50cb825401e8
zwibblTV <zwibbltv@gmail.com>
2018-04-22 21:20:27 +0200

    rank now with shovel giver is rank <= builder

--- plugin/dreamland/src/de/zwibbltv/dreamland/commands/CMDrank.java
+++ plugin/dreamland/src/de/zwibbltv/dreamland/commands/CMDrank.java
@@ -1,12 +1,15 @@
 package de.zwibbltv.dreamland.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
+import de.zwibbltv.dreamland.main.ItemBuilder;
 import de.zwibbltv.dreamland.main.Main;
 
 public class CMDrank implements CommandExecutor{
@@ -22,6 +25,32 @@ public class CMDrank implements CommandExecutor{
 					Player target = Bukkit.getPlayer(args[0]);					
 						if (target != null) {								
 							p.performCommand("pex user " + target.getName() + " group set " + args[1]);
+							if (target.hasPermission("dreamland.*") || target.hasPermission("dreamland.build.self") || target.hasPermission("dreamland.build.*")) {
+								if (!de.zwibbltv.dreamland.commands.CMDbuild.buildallowed.contains(target)) {
+									
+									ItemStack carrot = new ItemBuilder(Material.CARROT_ITEM).setDisplayName("§6Menu").build();
+									ItemStack golden_carrot = new ItemBuilder(Material.GOLDEN_CARROT).setDisplayName("§6Buy VIP").build();
+									ItemStack gold_spade = new ItemBuilder(Material.GOLD_SPADE).setDisplayName("§6Builder").build();
+									
+									target.getPlayer().getInventory().clear();
+									target.getPlayer().getEquipment().clear();							
+									target.getPlayer().getInventory().setItem(0, carrot);
+									target.getPlayer().getInventory().setItem(7, gold_spade);
+									target.getPlayer().getInventory().setItem(8, golden_carrot);
+								}
+							}
+							else if (!target.hasPermission("dreamland.*") && !target.hasPermission("dreamland.build.self") && !target.hasPermission("dreamland.build.*")) {
+								if (!de.zwibbltv.dreamland.commands.CMDbuild.buildallowed.contains(target)) {
+									
+									ItemStack carrot = new ItemBuilder(Material.CARROT_ITEM).setDisplayName("§6Menu").build();
+									ItemStack golden_carrot = new ItemBuilder(Material.GOLDEN_CARROT).setDisplayName("§6Buy VIP").build();
+									
+									target.getPlayer().getInventory().clear();
+									target.getPlayer().getEquipment().clear();							
+									target.getPlayer().getInventory().setItem(0, carrot);
+									target.getPlayer().getInventory().setItem(8, golden_carrot);
+								}
+							}
 						}		
 				} if (args.length == 1) {
 					Player target = Bukkit.getPlayer(sender.getName());
@@ -43,7 +72,33 @@ public class CMDrank implements CommandExecutor{
 				
 				Player target = Bukkit.getPlayer(args[0]);	
 					if (target != null) {							
-						con.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "pex user " + target.getName() + " group set " + args[1]);						
+						con.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "pex user " + target.getName() + " group set " + args[1]);	
+						if (target.hasPermission("dreamland.*") || target.hasPermission("dreamland.build.self") || target.hasPermission("dreamland.build.*")) {
+							if (!de.zwibbltv.dreamland.commands.CMDbuild.buildallowed.contains(target)) {
+								
+								ItemStack carrot = new ItemBuilder(Material.CARROT_ITEM).setDisplayName("§6Menu").build();
+								ItemStack golden_carrot = new ItemBuilder(Material.GOLDEN_CARROT).setDisplayName("§6Buy VIP").build();
+								ItemStack gold_spade = new ItemBuilder(Material.GOLD_SPADE).setDisplayName("§6Builder").build();
+								
+								target.getPlayer().getInventory().clear();
+								target.getPlayer().getEquipment().clear();							
+								target.getPlayer().getInventory().setItem(0, carrot);
+								target.getPlayer().getInventory().setItem(7, gold_spade);
+								target.getPlayer().getInventory().setItem(8, golden_carrot);
+							}
+						}
+						else if (!target.hasPermission("dreamland.*") && !target.hasPermission("dreamland.build.self") && !target.hasPermission("dreamland.build.*")) {
+							if (!de.zwibbltv.dreamland.commands.CMDbuild.buildallowed.contains(target)) {
+								
+								ItemStack carrot = new ItemBuilder(Material.CARROT_ITEM).setDisplayName("§6Menu").build();
+								ItemStack golden_carrot = new ItemBuilder(Material.GOLDEN_CARROT).setDisplayName("§6Buy VIP").build();
+								
+								target.getPlayer().getInventory().clear();
+								target.getPlayer().getEquipment().clear();							
+								target.getPlayer().getInventory().setItem(0, carrot);
+								target.getPlayer().getInventory().setItem(8, golden_carrot);
+							}
+						}
 					}											
 			} if (args.length == 0) {
 				con.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "pex groups list");
