111e1bbae1731f9f9b1a99fcdf6497a3fe6538d7
Steven Paw <kontakt@stevenpaw.de>
2018-04-28 14:05:39 +0200

    Added /audio as startcommand

--- plugin/dreamland/src/de/zwibbltv/dreamland/utils/Tablist.java
+++ plugin/dreamland/src/de/zwibbltv/dreamland/utils/Tablist.java
@@ -8,25 +8,25 @@ import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public class Tablist {
 	
-	private static Scoreboard sb;
+	private static Scoreboard sb_tab;
 	
 	public static void setScoreboard() {
 		
-		sb = Bukkit.getScoreboardManager().getNewScoreboard();
+		sb_tab = Bukkit.getScoreboardManager().getNewScoreboard();
 		
-		sb.registerNewTeam("000Owner");
-		sb.registerNewTeam("001Admin");
-		sb.registerNewTeam("002Builder");
-		sb.registerNewTeam("003VIP");
-		sb.registerNewTeam("004Member");
-		sb.registerNewTeam("005Guest");
+		sb_tab.registerNewTeam("000Owner");
+		sb_tab.registerNewTeam("001Admin");
+		sb_tab.registerNewTeam("002Builder");
+		sb_tab.registerNewTeam("003VIP");
+		sb_tab.registerNewTeam("004Member");
+		sb_tab.registerNewTeam("005Guest");
 		
-		sb.getTeam("000Owner").setPrefix("§2Owner §6|§r ");
-		sb.getTeam("001Admin").setPrefix("§9Admin §6|§r ");
-		sb.getTeam("002Builder").setPrefix("§5Builder §6|§r ");
-		sb.getTeam("003VIP").setPrefix("§eVIP§6 |§r ");
-		sb.getTeam("004Member").setPrefix("§3Member§6 |§r ");
-		sb.getTeam("005Guest").setPrefix("§7Guest§6 |§7 ");
+		sb_tab.getTeam("000Owner").setPrefix("§2Owner §6|§r ");
+		sb_tab.getTeam("001Admin").setPrefix("§9Admin §6|§r ");
+		sb_tab.getTeam("002Builder").setPrefix("§5Builder §6|§r ");
+		sb_tab.getTeam("003VIP").setPrefix("§eVIP§6 |§r ");
+		sb_tab.getTeam("004Member").setPrefix("§3Member§6 |§r ");
+		sb_tab.getTeam("005Guest").setPrefix("§7Guest§6 |§7 ");
 		
 		for(Player all : Bukkit.getOnlinePlayers()) {
 			setTeams(all);
@@ -57,9 +57,8 @@ public class Tablist {
 		else if(PermissionsEx.getUser(p).inGroup("Guest")) {
 			team = "005Guest";
 		}
-		sb.getTeam(team).addPlayer(p);
-		p.setScoreboard(sb);
-		
+		sb_tab.getTeam(team).addPlayer(p);
+		p.setScoreboard(sb_tab);
 	}
 
 }
