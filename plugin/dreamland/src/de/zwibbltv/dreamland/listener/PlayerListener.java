25ef8c5bef6d206590c00dbab41271ff12a3bb28
Steven Paw <kontakt@stevenpaw.de>
2018-07-24 18:16:25 +0200

    Updated Resourcepack

--- plugin/dreamland/src/de/zwibbltv/dreamland/listener/PlayerListener.java
+++ plugin/dreamland/src/de/zwibbltv/dreamland/listener/PlayerListener.java
@@ -249,7 +249,7 @@ public class PlayerListener implements Listener {
 		if(PlayerConfig.getResourcepack(p) != true && PlayerConfig.getRuntime(p) >= 1)
 		{
 			p.sendMessage("Sending Resourcepack");
-			p.setResourcePack("https://www.dropbox.com/s/5kou870hjc22kd1/Dreamland24-07-18.zip?dl=1");
+			p.setResourcePack("https://www.dropbox.com/s/0o0nxnc43e3gav1/Dreamland24-07-18c.zip?dl=1");
 			try {
 				PlayerConfig.Resourcepackactive(p, true);
 			} catch (IOException error) {
