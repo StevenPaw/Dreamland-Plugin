2ea680916d37fa497a16938a6765226354afb60a
ZwibblTV <zwibbltv@gmail.com>
2018-07-25 21:58:04 +0200

    mini rl cmd

--- plugin/dreamland/src/de/zwibbltv/dreamland/listener/PlayerListener.java
+++ plugin/dreamland/src/de/zwibbltv/dreamland/listener/PlayerListener.java
@@ -247,7 +247,7 @@ public class PlayerListener implements Listener {
 		if(PlayerConfig.getResourcepack(p) != true && PlayerConfig.getRuntime(p) >= 1)
 		{
 			p.sendMessage("Sending Resourcepack");
-			p.setResourcePack("https://www.dropbox.com/s/0o0nxnc43e3gav1/Dreamland24-07-18c.zip?dl=1");
+			p.setResourcePack("https://www.dropbox.com/s/6sdbbxqeocjcsxi/Dreamland25-07-18.zip?dl=1");
 			try {
 				PlayerConfig.Resourcepackactive(p, true);
 			} catch (IOException error) {
